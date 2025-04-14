5bd9a8c5c7c4e14012302a83ca120759e7cb1bb9
Guilherme Mattos <guilhermevsmattos@outlook.com>
2025-04-14 17:44:14 -0300

    feat: adding action do send link and do login

---
+++ app/(auth)/login/actions.ts
@@ -0,0 +1,36 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export type LoginState = {
+  success: null | boolean;
+  message?: string;
+}
+
+export async function login(previousState: LoginState, formData: FormData) {
+  const supabase = await createClient()
+
+  const email = formData.get('email') as string
+
+  const { error } = await supabase.auth.signInWithOtp({
+    email,
+    options: {
+      emailRedirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/confirm`
+    }
+  })
+
+  if (error) {
+    console.log('erro')
+    return {
+      success: false,
+      message: error.message
+    }
+  }
+  
+  console.log('deu certo')
+  return {
+    success: true,
+    message: "Email enviado!"
+  }
+}
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import { useActionState } from "react"
 import { Button } from "./ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
+import { login, LoginState } from "@/app/(auth)/login/actions"
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert"
+import { Loader, MessageCircle } from "lucide-react"
 
 export default function LoginForm() {
+  const [state, formAction, pending] = useActionState<LoginState, FormData>(
+    login,
+    {
+      success: null,
+      message: ""
+    }
+  );
+
   return (
     <Card className="w-full max-w-md p-6">
       <CardHeader>
@@ -15,13 +27,39 @@ export default function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form action="">
+        <form action={formAction}>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input id="email" type="email" name="email" placeholder="maria@gmail.com" required />
             </div>
+
+            {state.success === true && (
+              <Alert className="text-muted-foreground">
+                <MessageCircle className="h-4 w-4 !text-green-600" />
+                <AlertTitle className="text-gray-50">
+                  Email enviado!
+                </AlertTitle>
+                <AlertDescription>
+                  Confira seu inbox para acessar o link de login.
+                </AlertDescription>
+              </Alert>
+            )}
+
+            {state.success === false && (
+              <Alert className="text-muted-foreground">
+                <MessageCircle className="h-4 w-4 !text-red-600" />
+                <AlertTitle className="text-gray-50">
+                  Erro!
+                </AlertTitle>
+                <AlertDescription>
+                  Ocorreu um erro ao enviar o link de login. Entre em contato com o suporte!
+                </AlertDescription>
+              </Alert>
+            )}
+
             <Button type="submit" className="w-full">
+              {pending && <Loader className="animate-spin" />}
               Login
             </Button>
           </div>
